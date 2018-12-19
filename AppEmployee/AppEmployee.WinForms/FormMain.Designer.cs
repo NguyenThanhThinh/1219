d450abc0e6518af8b4f13d840990ed935ff6e100
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-12-20 00:42:23 +0700

    add ui

--- AppEmployee/AppEmployee.WinForms/FormMain.Designer.cs
+++ AppEmployee/AppEmployee.WinForms/FormMain.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.dtpBirthDay = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,23 +47,97 @@
             this.dgvEmployee.Size = new System.Drawing.Size(783, 179);
             this.dgvEmployee.TabIndex = 0;
             // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(44, 27);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(131, 20);
+            this.txtNo.TabIndex = 1;
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNo.Location = new System.Drawing.Point(12, 27);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(26, 17);
+            this.lblNo.TabIndex = 2;
+            this.lblNo.Text = "No";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(263, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FirstName";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(341, 27);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(131, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(560, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LastName";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(638, 27);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(131, 20);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // dtpBirthDay
+            // 
+            this.dtpBirthDay.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBirthDay.Location = new System.Drawing.Point(44, 76);
+            this.dtpBirthDay.Name = "dtpBirthDay";
+            this.dtpBirthDay.Size = new System.Drawing.Size(131, 20);
+            this.dtpBirthDay.TabIndex = 7;
+            // 
             // frmEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtpBirthDay);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.txtNo);
             this.Controls.Add(this.dgvEmployee);
             this.Name = "frmEmployee";
             this.Text = "FormMain";
             this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.DateTimePicker dtpBirthDay;
     }
 }
 
