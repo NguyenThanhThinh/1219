b1c2b0356354e97665bfba72aafa007ce216342d
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-12-19 23:23:44 +0700

    add entities

---
+++ AppEmployee/AppEmployee.Domain/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace AppEmployee.Domain.Entities
+{
+    public class Department : Entity<int>, IDateTracking
+    {
+        [Required]
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; }
+
+        public ICollection<Employee> Employeee { get; set; } = new HashSet<Employee>();
+    }
+}
---
+++ AppEmployee/AppEmployee.Domain/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppEmployee.Domain.Entities
+{
+    public class Employee : Entity<int>, IDateTracking
+    {
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+
+        public string LastName { get; set; }
+
+        public bool Sex { get; set; }
+
+        public DateTime? BirthDay { get; set; }
+
+        [StringLength(500)]
+        public string Address { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
